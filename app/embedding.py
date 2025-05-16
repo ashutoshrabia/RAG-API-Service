65d006ee509084aecbc0a76712c16b33922a5d9f
Ashutosh Rabia <121341226+ashutoshrabia@users.noreply.github.com>
2025-05-16 12:01:57 +0530

    Update embedding.py

--- app/embedding.py
+++ app/embedding.py
@@ -3,8 +3,6 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def embed_text(chunks: list[str]) -> list[list[float]]:
-    """
-    Generate embeddings for a list of text chunks.
-    """
+
     embeddings = model.encode(chunks, show_progress_bar=False)
     return embeddings.tolist()
